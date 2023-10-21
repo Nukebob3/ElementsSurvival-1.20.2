a8f4220de73d78e1140c01ee77ab6024fe963dff
Nukebob3 <david24nel@gmail.com>
2023-10-22 10:20:31 +1100

    Initial Commit

---
+++ src/main/java/net/nukebob/elements/ElementsSurvivalClient.java
@@ -0,0 +1,10 @@
+package net.nukebob.elements;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementsSurvivalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
