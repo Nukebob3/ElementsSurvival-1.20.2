cba4c0638cacb7c5fcc375b0dde95b23c6573ec9
Nukebob3 <david24nel@gmail.com>
2023-10-22 11:29:45 +1100

    Added Element Stone

---
+++ src/main/java/net/nukebob/elements/item/ModItemGroups.java
@@ -0,0 +1,24 @@
+package net.nukebob.elements.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.nukebob.elements.ElementsSurvival;
+
+public class ModItemGroups {
+    public static final ItemGroup ELEMENTS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(ElementsSurvival.MOD_ID, "elements"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.elements"))
+                    .icon(() -> new ItemStack(ModItems.ELEMENT_STONE)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.ELEMENT_STONE);
+                        entries.add(ModItems.AMONGUS);
+                    }).build());
+
+    public static void registerItemGroups() {
+        ElementsSurvival.LOGGER.info("Registering item groups for "+ElementsSurvival.MOD_ID);
+    }
+}
---
+++ src/main/java/net/nukebob/elements/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.nukebob.elements.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.nukebob.elements.ElementsSurvival;
+
+public class ModItems {
+    public static final Item ELEMENT_STONE = registerItem("element_stone", new Item(new FabricItemSettings()));
+    public static final Item AMONGUS = registerItem("amongus", new Item(new FabricItemSettings()));
+
+    private static void addItemsToToolsItemGroup(FabricItemGroupEntries entries) {
+        entries.add(ELEMENT_STONE);
+        entries.add(AMONGUS);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(ElementsSurvival.MOD_ID, name),item);
+    }
+
+    public static void registerModItems() {
+        ElementsSurvival.LOGGER.info("Registering Mod Items for "+ElementsSurvival.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::addItemsToToolsItemGroup);
+    }
+}
